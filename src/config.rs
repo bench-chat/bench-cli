40bd2bcacda6d14229ae4fd9134895e930aa1270
John Beatty <john@exposit.ai>
2025-02-10 21:46:38 -0800

    update the production url to bench.chat

--- src/config.rs
+++ src/config.rs
@@ -27,7 +27,7 @@ impl Config {
     pub fn new(env: &Environment) -> anyhow::Result<Self> {
         let base_url = match env {
             Environment::Local => Url::parse("http://localhost:3001")?,
-            Environment::Production => Url::parse("https://chatbench.dev")?,
+            Environment::Production => Url::parse("https://bench.chat")?,
             Environment::Custom(url) => Url::parse(url)?,
         };
         Ok(Self { base_url })
